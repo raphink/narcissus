776aa5058b9db96fc1cbbf9259f2cafde54e2eb2
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-11 15:24:36 +0200

    Add Hosts

---
+++ hosts.go
@@ -0,0 +1,23 @@
+package narcissus
+
+// Hosts maps a /etc/hosts file
+type Hosts struct {
+	Comments []struct {
+		Comment string `path:"."`
+	} `path:"#comment"`
+	Hosts []Host `type:"seq"`
+}
+
+// Host maps an Hosts entry
+type Host struct {
+	IPAddress string `path:"ipaddr"`
+	Canonical string `path:"canonical"`
+	Comment   string `path:"#comment"`
+}
+
+// NewHosts returns a new Hosts structure
+func (n *Narcissus) NewHosts() (h *Hosts, err error) {
+	h = &Hosts{}
+	err = n.Parse(h, "/files/etc/hosts")
+	return
+}
---
+++ hosts_test.go
@@ -0,0 +1,48 @@
+package narcissus
+
+import (
+	"testing"
+
+	"honnef.co/go/augeas"
+)
+
+func TestHosts(t *testing.T) {
+	aug, err := augeas.New("/home/raphink/go/src/github.com/raphink/narcissus/fakeroot", "", augeas.None)
+	if err != nil {
+		t.Fatal("Failed to create Augeas handler")
+	}
+	n := New(&aug)
+
+	// Test one fstab
+	host := &Host{}
+	err = n.Parse(host, "/files/etc/hosts/1")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if host.IPAddress != "127.0.0.1" {
+		t.Errorf("Expected IP to be 127.0.0.1, got %s", host.IPAddress)
+	}
+}
+
+func TestHost(t *testing.T) {
+	aug, err := augeas.New("/home/raphink/go/src/github.com/raphink/narcissus/fakeroot", "", augeas.None)
+	if err != nil {
+		t.Fatal("Failed to create Augeas handler")
+	}
+	n := New(&aug)
+
+	// Test one fstab
+	hosts, err := n.NewHosts()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if len(hosts.Hosts) != 5 {
+		t.Errorf("Expected 3 hosts, got %v", len(hosts.Hosts))
+	}
+
+	if hosts.Hosts[2].IPAddress != "::1" {
+		t.Errorf("Expected IP to be ::1, got %s", hosts.Hosts[2].IPAddress)
+	}
+}
