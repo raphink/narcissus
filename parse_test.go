9d477a0b0ceb63bba152b82a8e0de8dfb94c93f7
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-11 16:33:04 +0200

    Add test for field not found (currently failing)

--- parse_test.go
+++ parse_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 type foo struct {
+	A string `path:"a"`
 }
 
 func TestParseNotAPtr(t *testing.T) {
@@ -33,5 +34,15 @@ func TestParseNotAStruct(t *testing.T) {
 
 	if err.Error() != "not a struct" {
 		t.Errorf("Expected error not a struct, got %s", err.Error())
+	}
+}
+
+func TestParseFieldNotFound(t *testing.T) {
+	n := New(&augeas.Augeas{})
+	t.Skip("This causes a segfault with the Augeas lib. Open a bug!")
+	err := n.Parse(&foo{}, "/files/some/path")
+
+	if err == nil {
+		t.Error("Expected an error, got nothing")
 	}
 }
