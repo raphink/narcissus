7f12021542f9d23856da5eba3f866d4681295eaf
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-11 20:28:32 +0200

    Add Write() without implementation yet

---
+++ write.go
@@ -0,0 +1,8 @@
+package narcissus
+
+import "log"
+
+// Write writes a structure pointer to the Augeas tree
+func (n *Narcissus) Write(val interface{}) error {
+	log.Fatal("Write is not implemented yet")
+}
