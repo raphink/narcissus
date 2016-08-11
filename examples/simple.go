7dda1522a11798b750b750aefaec12106cfbf0ff
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-11 10:52:51 +0200

    Add examples

---
+++ examples/simple.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+
+	"github.com/raphink/narcissus"
+	"honnef.co/go/augeas"
+)
+
+type group struct {
+	Name     string `path:"." value-from:"label"`
+	Password string `path:"password"`
+	Gid      int    `path:"gid"`
+}
+
+func main() {
+	aug, err := augeas.New("/", "", augeas.None)
+	if err != nil {
+		log.Fatal("Failed to create Augeas handler")
+	}
+	n := narcissus.New(&aug)
+
+	user := &narcissus.PasswdUser{}
+	err = n.Parse(user, "/files/etc/passwd/root")
+	if err != nil {
+		log.Fatalf("Expected no error, got %v", err)
+	}
+
+	log.Printf("Uid=%v", user.Shell)
+
+	group := &group{}
+	err = n.Parse(group, "/files/etc/group/docker")
+	if err != nil {
+		log.Fatalf("Expected no error, got %v", err)
+	}
+
+	log.Printf("Uid=%v", group.Gid)
+}
