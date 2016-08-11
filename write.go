1630053a8bce838a6a28fb1a32838b352f76fa67
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-12 00:12:34 +0200

    writeField()

--- write.go
+++ write.go
@@ -21,5 +21,21 @@ func (n *Narcissus) Write(val interface{}) error {
 }
 
 func (n *Narcissus) writeStruct(ref reflect.Value, path string) error {
+	refType := ref.Type()
+	for i := 0; i < refType.NumField(); i++ {
+		if refType.Field(i).Name == "augeasPath" {
+			// Ignore the special `augeasPath` field
+			continue
+		}
+		err := n.writeField(ref.Field(i), refType.Field(i), path)
+		if err != nil {
+			return fmt.Errorf("failed to write field %s: %v", refType.Field(i).Name, err)
+		}
+	}
+
+	return nil
+}
+
+func (n *Narcissus) writeField(field reflect.Value, fieldType reflect.StructField, path string) error {
 	return fmt.Errorf("Write() is not implemented yet")
 }
