7ce28cf3c0f7c7d77ff4a2c84881403e5d3c93ae
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-11 15:51:41 +0200

    Add services

---
+++ services_test.go
@@ -0,0 +1,46 @@
+package narcissus
+
+import (
+	"testing"
+
+	"honnef.co/go/augeas"
+)
+
+func TestService(t *testing.T) {
+	aug, err := augeas.New("/home/raphink/go/src/github.com/raphink/narcissus/fakeroot", "", augeas.None)
+	if err != nil {
+		t.Fatal("Failed to create Augeas handler")
+	}
+	n := New(&aug)
+
+	// Test one service
+	service, err := n.NewService("ssh", "tcp")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if service.Port != 22 {
+		t.Errorf("Expected port 22, got %v", service.Port)
+	}
+
+	if service.Comment != "SSH Remote Login Protocol" {
+		t.Errorf("Expected SSH comment, got %s", service.Comment)
+	}
+}
+
+func TestServices(t *testing.T) {
+	aug, err := augeas.New("/home/raphink/go/src/github.com/raphink/narcissus/fakeroot", "", augeas.None)
+	if err != nil {
+		t.Fatal("Failed to create Augeas handler")
+	}
+	n := New(&aug)
+
+	services, err := n.NewServices()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if len(services.Services) != 557 {
+		t.Errorf("Expected 557 services, got %v", len(services.Services))
+	}
+}
