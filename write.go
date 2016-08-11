a90b0b8b64e89cbfad273b0d1c81f8095e034d45
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-12 00:08:51 +0200

    Write() skeleton

--- write.go
+++ write.go
@@ -1,8 +1,25 @@
 package narcissus
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 // Write writes a structure pointer to the Augeas tree
 func (n *Narcissus) Write(val interface{}) error {
+	ref, err := structRef(val)
+	if err != nil {
+		return fmt.Errorf("invalid interface: %v", err)
+	}
+
+	path, err := getPath(ref)
+	if err != nil {
+		return fmt.Errorf("undefined path: %v", err)
+	}
+
+	return n.writeStruct(ref, path)
+}
+
+func (n *Narcissus) writeStruct(ref reflect.Value, path string) error {
 	return fmt.Errorf("Write() is not implemented yet")
 }
