21ab462c5b1fc3ae65302d2d06186ca620e4fa91
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-12 11:15:48 +0200

    Test adding a new user

--- passwd_test.go
+++ passwd_test.go
@@ -52,7 +52,6 @@ func TestParsePasswdUser(t *testing.T) {
 }
 
 func TestWritePasswd(t *testing.T) {
-	// FIXME: use augeas.SaveNewFile, but it is broken?
 	aug, err := augeas.New(fakeroot, "", augeas.SaveNewFile)
 	if err != nil {
 		t.Fatal("Failed to create Augeas handler")
@@ -120,5 +119,72 @@ func TestWritePasswd(t *testing.T) {
 }
 
 func TestWritePasswdNewUser(t *testing.T) {
-	t.Skip("TO BE IMPLEMENTED")
+	aug, err := augeas.New(fakeroot, "", augeas.SaveNewFile)
+	if err != nil {
+		t.Fatal("Failed to create Augeas handler")
+	}
+	n := New(&aug)
+
+	// Cleanup
+	os.Remove(fakeroot + "/etc/passwd.augnew")
+
+	passwd, err := n.NewPasswd()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	err = n.Write(passwd)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	err = aug.Save()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	errStr, _ := aug.Get("/augeas//error/message")
+	if errStr != "" {
+		t.Errorf("Failed with %s", errStr)
+	}
+
+	// check that file is unchanged
+	if f, err := os.Stat(fakeroot + "/etc/passwd.augnew"); err == nil && f.Mode().IsRegular() {
+		t.Errorf("Expected augnew file to be absent, was present")
+	}
+
+	user, err := n.NewPasswdUser("foo")
+	user.UID = 314
+	user.GID = 314
+	user.Name = "Foo Bar"
+	user.Home = "/home/foo"
+	user.Shell = "/bin/sh"
+	user.Password = "XXX"
+	passwd.Users["foo"] = *user
+
+	err = n.Write(passwd)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	err = aug.Save()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	errStr, _ = aug.Get("/augeas//error/message")
+	if errStr != "" {
+		t.Errorf("Failed with %s", errStr)
+	}
+
+	// check that file is changed
+	expectedDiff := `--- orig
++++ new
+@@ -42,0 +43 @@
++foo:XXX:314:314:Foo Bar:/home/foo:/bin/sh
+`
+	diff, err := diffNewContent("/etc/passwd")
+	if err != nil {
+		t.Errorf("Failed to compute diff: %v", err)
+	} else if diff != expectedDiff {
+		t.Errorf("Expected diff %s, got %s", expectedDiff, diff)
+	}
 }
