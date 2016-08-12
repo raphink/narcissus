dd1c443ed235d797a5a7de575499c59e07205ab9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-12 14:17:19 +0200

    Lint: simplify if/else

--- util.go
+++ util.go
@@ -25,15 +25,14 @@ func getPath(ref reflect.Value) (string, error) {
 	refType := ref.Type()
 	if pType, ok := refType.FieldByName("augeasPath"); ok {
 		p := ref.FieldByName("augeasPath")
-		if pp := p.String(); pp == "" {
+		pp := p.String()
+		if pp == "" {
 			if defaultP := pType.Tag.Get("default"); defaultP != "" {
 				return defaultP, nil
-			} else {
-				return "", fmt.Errorf("no augeasPath value and no default")
 			}
-		} else {
-			return pp, nil
+			return "", fmt.Errorf("no augeasPath value and no default")
 		}
+		return pp, nil
 	}
 	return "", fmt.Errorf("no augeasPath field")
 }
